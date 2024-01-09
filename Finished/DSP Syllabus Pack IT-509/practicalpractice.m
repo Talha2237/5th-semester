a9ce566d001e97f3e563b3ece1b869695b9c4620
talha <coldstorm2237@gmail.com>
2024-01-09 17:03:42 +0500

    5th semester

---
+++ Finished/DSP Syllabus Pack IT-509/practicalpractice.m
@@ -0,0 +1,26 @@
+a = 1;
+b = 23;
+c = a + b;
+d = cos(a);
+cos(a)
+
+
+A = [1,2,3,4,5]
+
+B = [1,2,5;6,1,2;7,2,3]
+
+C = zeros(3,3)
+
+D = B + C
+format short
+f = cos(D)
+
+format short
+E = f*inv(f)
+
+sqrt(4)
+
+Com = [3+4i, 4+3j; -i, 10j]
+
+E(1,2)
+
